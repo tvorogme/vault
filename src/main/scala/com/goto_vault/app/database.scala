06a04d4a413af79d8c7d11f318aa17bfa5beecdb
Andrew Tvorozhkov <tvorog@tvorog.me>
2017-06-21 01:44:59 +0300

    Add working bank model

--- src/main/scala/com/goto_vault/app/database.scala
+++ src/main/scala/com/goto_vault/app/database.scala
@@ -1,84 +1,36 @@
 package com.goto_vault.app
 
-class database {
-  def create_account(): Unit ={
-
-  }
-
-  def add_transaction(): Unit ={
-
-  }
-
-  def update_account(): Unit ={
-
-  }
-
-  def del_account(): Unit ={
-
-  }
-}
 
 import org.scalatra.{ScalatraBase, FutureSupport, ScalatraServlet}
-
 import slick.jdbc.H2Profile.api._
-
 import scala.concurrent.ExecutionContext.Implicits.global
 
 object Tables {
 
-  // Definition of the SUPPLIERS table
-  class Suppliers(tag: Tag) extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
-    def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
-    def name = column[String]("SUP_NAME")
-    def street = column[String]("STREET")
-    def city = column[String]("CITY")
-    def state = column[String]("STATE")
-    def zip = column[String]("ZIP")
+  // Definition of the Accounts table
+  class Accounts(tag: Tag) extends Table[(Int, String, Int)](tag, "Accounts") {
 
-    // Every table needs a * projection with the same type as the table's type parameter
-    def * = (id, name, street, city, state, zip)
+    def id = column[Int]("ID", O.PrimaryKey)
+    def name = column[String]("NAME")
+    def balance = column[Int]("BALANCE")
+
+    def * = (id, name, balance)
   }
 
-  // Definition of the COFFEES table
-
-  // Table query for the SUPPLIERS table, represents all tuples
-  val suppliers = TableQuery[Suppliers]
+  val Accounts = TableQuery[Accounts]
 
 
-  // DBIO Action which runs several queries inserting sample data
   val insertSupplierAndCoffeeData = DBIO.seq(
-    Tables.suppliers += (101, "Acme, Inc.", "99 Market Street", "Groundsville", "CA", "95199"),
-    Tables.suppliers += (49, "Superior Coffee", "1 Party Place", "Mendocino", "CA", "95460"),
-    Tables.suppliers += (150, "The High Ground", "100 Coffee Lane", "Meadows", "CA", "93966"),
+    Tables.Accounts += (1, "Andrew Tvorozhkov", 0),
+    Tables.Accounts += (1, "Grisha Belogorov", 0)
   )
 
-  // DBIO Action which creates the schema
-  val createSchemaAction = (suppliers.schema).create
-
-  // DBIO Action which drops the schema
-  val dropSchemaAction = (suppliers.schema).drop
-
-  // Create database, composing create schema and insert sample data actions
+  val createSchemaAction = (Accounts.schema).create
+  val dropSchemaAction = (Accounts.schema).drop
   val createDatabase = DBIO.seq(createSchemaAction)
-
 }
 
-trait SlickRoutes extends ScalatraBase with FutureSupport {
-
-  def db: Database
-
-  get("/db/create-db") {
-    db.run(Tables.createDatabase)
-  }
-
-  get("/db/drop-db") {
-    db.run(Tables.dropSchemaAction)
-  }
-
-}
-
-class SlickApp(val db: Database) extends ScalatraServlet with FutureSupport with SlickRoutes {
-
+class SlickApp(val db: Database) extends ScalatraServlet with FutureSupport {
   protected implicit def executor = scala.concurrent.ExecutionContext.Implicits.global
 
 }
