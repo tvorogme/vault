3e0b14d576325c904c14c45bd3dcc33e1d4ae304
Greg <gvaisberg@yandex.ru>
2017-06-20 23:36:11 +0300

    Account

---
+++ src/main/scala/com/goto_vault/app/bank.scala
@@ -0,0 +1,33 @@
+package com.goto_vault.app
+
+import java.util.UUID
+import java.util.Calendar
+
+
+class Transaction(val from: UUID, val to: UUID, val amount: Double, val operation: Boolean) {
+  val datetime: = Calendar.getInstance()
+}
+
+class Account(var balance: Double = 0.0) {
+  // Uid аккаунта
+  val uid: UUID = java.util.UUID.randomUUID
+
+  // Добавление денег на аккаунт
+  def deposit(from: UUID, amount: Double) {
+    // Добавить денег
+    val tmp_transaction: Transaction = new Transaction(from, this.uid, amount, 1)
+    balance += amount
+  }
+
+  //вывод средств со счета
+  def take_money(to: UUID, amount: Double): Boolean = {
+    if (amount > balance)
+      false
+    else {
+      val tmp_transaction: Transaction = new Transaction(this.uid.to, amount, 0)
+      balance -= amount
+
+      true
+    }
+  }
+}
