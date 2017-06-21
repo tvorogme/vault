73970495372ca73713c6aa8d44c4c5d6b6fce0cd
Andrew Tvorozhkov <tvorog@tvorog.me>
2017-06-21 10:58:25 +0300

    Add working account

--- src/main/scala/com/goto_vault/app/Account.scala
+++ src/main/scala/com/goto_vault/app/Account.scala
@@ -1,15 +1,25 @@
 package com.goto_vault.app
+
 import slick.jdbc.H2Profile.api._
 
 /**
   * Created by acadabus on 6/21/17.
   */
-class Account(tag: Tag) extends Table[(Int, String, Double,String, String)](tag, "Accounts") {
 
-    def id = column[Int]("ID", O.PrimaryKey)
-    def name = column[String]("NAME")
-    def balance = column[Double]("BALANCE")
-    def password = column[String]("PASS")
-    def email = column[String]("EMAIL")
-    def * = (id, name, balance, password, email)
+
+case class Account(id: Int, name: String, balance: Double, password: String, email: String)
+
+class AccountTable(tag: Tag) extends Table[(Int, String, Double, String, String)](tag, "Accounts") {
+
+  def id = column[Int]("ID", O.PrimaryKey)
+
+  def name = column[String]("NAME")
+
+  def balance = column[Double]("BALANCE")
+
+  def password = column[String]("PASS")
+
+  def email = column[String]("EMAIL")
+
+  def * = (id, name, balance, password, email)
 }
