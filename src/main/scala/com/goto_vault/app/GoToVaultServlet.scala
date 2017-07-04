08955a5b7a4c2ef2705666775f9ad0546c0a4062
Andrew Tvorozhkov <tvorog@tvorog.me>
2017-07-05 01:16:35 +0300

    New style in admin

--- src/main/scala/com/goto_vault/app/GoToVaultServlet.scala
+++ src/main/scala/com/goto_vault/app/GoToVaultServlet.scala
@@ -61,8 +61,12 @@ class GoToVaultServlet extends ZvezdochkaStack with AuthenticationSupport {
 
     if (user.head.admin) {
       Setup.all_accounts(mutable = true) +
-        s"""<style>li{    width: 370px; height: 200px;
-           |    float: left;}</style>
+        s"""<style>li{    width: 370px;
+           |    height: 200px;
+           |    float: left;
+           |    padding: 25px;
+           |    text-align: center;
+           |    list-style-type: none;}</style>
            |<form action='${Setup.prefix}admin/add_good' method='post'>
            |<input type='text' name='good_name'> <br> <br>
            |<input type='text' name='good_price'> <br> <br>
